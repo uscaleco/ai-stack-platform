8f4db63c72f7c96ab43ccb177a364584dcf6c8c4
billy <amramathieu@gmail.com>
2025-06-05 13:37:35 +0200

    test front end test workflow

--- frontend/app/auth/callback/route.ts
+++ frontend/app/auth/callback/route.ts
@@ -2,6 +2,7 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
+/** epmrpomw **/
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
